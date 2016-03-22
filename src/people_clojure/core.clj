5e209e2c6e2f92814a7e32c4f6fc347b09c6de62
BrandenSandahl <branden157@gmail.com>
2016-03-22 12:13:31 -0400

    added all the functions to do the things we want to do with the stuff

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,5 +1,22 @@
 (ns people-clojure.core
+  (:require [clojure.string :as str] ;this brings in a new namespace
+            [clojure.walk :as walk]) ;brings in walk lib 
   (:gen-class))
 
 (defn -main []
-  (println "Hello, World!"))
+  (let [people (slurp "people.csv") ;slurp reads in a file
+        people (str/split-lines people) ;calls split-lines from the clojure.string lib
+        people (map (fn [line]
+                      (str/split line #","))
+                    people)
+        header (first people)
+        people (rest people)
+        people (map (fn [line]
+                      (apply hash-map (interleave header line)))
+                    people)
+        people (walk/keywordize-keys people)
+        people (filter (fn [line]
+                         (= (:country line) "Brazil"))
+                 people)]
+    (spit "filtered_people.edn" (pr-str people))   ;this is a file writer
+    people))
